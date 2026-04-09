7b297da63893c1962630b8fde016392c4fac3d52
Kongnyu Roger <kongnyuroger711@gmail.com>
2026-04-09 15:22:01 +0100

    added interactive to my cli

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 
 const version = '0.0.1';
 void main(List<String> arguments) {
@@ -5,11 +6,33 @@ void main(List<String> arguments) {
     printUsage();
   }else if (arguments.first == 'version'){
     print('cli version is $version');
-  } else {
+  }else if (arguments.first == 'search'){
+    final inputArgs= arguments.length > 1 ? arguments.sublist(1) :<String>[];
+    searchWikipidia(inputArgs);
+    }
+    else {
     printUsage();
   }
 }
 
+void searchWikipidia(List<String> arguments){ 
+       final String articleTitle;
+
+
+  if (arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into a single string.
+    articleTitle = arguments.join(' ');
+  }
+
+  print ('looking for an article about $articleTitle, please wait');
+  print('here you go ');
+  print('pretend this is and article about $articleTitle. enjoy;');
+}
+
 void printUsage(){
-  print("the following comand are valid: 'help', 'version', 'search'");
+  print("the following comand are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
 }
